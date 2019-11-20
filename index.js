7901d2d457e10581eede7d9cca119c00d10f7660
Suhan Moon <lunyx@me.com>
2019-11-21 00:14:31 +0900

    manual set

--- index.js
+++ index.js
@@ -1,51 +1,26 @@
 import { NativeModules } from "react-native";
 
-const { ANKakaoLogin, ANKakaoLink, ANKakaoChannel } = NativeModules;
+const { ARNKakaoLink } = NativeModules;
 
 export const link = {
   sendFeed: object => {
-    ANKakaoLink.sendFeed(object);
+    ARNKakaoLink.sendFeed(object);
   },
   sendList: object => {
-    ANKakaoLink.sendList(object);
+    ARNKakaoLink.sendList(object);
   },
   sendLocation: object => {
-    ANKakaoLink.sendLocation(object);
+    ARNKakaoLink.sendLocation(object);
   },
   sendCommerce: object => {
-    ANKakaoLink.sendCommerce(object);
+    ARNKakaoLink.sendCommerce(object);
   },
   sendText: object => {
-    ANKakaoLink.sendText(object);
+    ARNKakaoLink.sendText(object);
   },
   sendURL: object => {
-    ANKakaoLink.sendURL(object);
+    ARNKakaoLink.sendURL(object);
   }
 };
 
-export const login = {
-  getAccessToken: () => {
-    ANKakaoLogin.getAccessToken();
-  },
-  login: () => {
-    ANKakaoLogin.login();
-  },
-  logout: () => {
-    ANKakaoLogin.logout();
-  }
-};
-
-export const channel = {
-  addFriend: id => {
-    ANKakaoChannel.addFriend(id);
-  },
-  chat: id => {
-    ANKakaoChannel.chat(id);
-  }
-};
-
-export default {
-  link,
-  login,
-  channel
-};
+export default link;
